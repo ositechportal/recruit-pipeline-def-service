5437f513362bcbb7c3a7614056477c99e737fc0f
ajinkya <ajinkya_kakade@gap.com>
2017-03-01 23:16:26 +0530

    Added default service jobs for master and branch

---
+++ src/main/java/com/ositech/job_dsl_branch.groovy
@@ -0,0 +1,83 @@
+job('Employee-Service-Job') {
+    scm {
+        git {
+            remote {
+                url 'https://github.com/ositechportal/employee-service.git'
+            }
+            extensions {
+                wipeOutWorkspace()
+            }
+            branch '*/OSI*'
+        }
+    }
+
+    steps {
+        gradle {
+            tasks('clean')
+            tasks('test')
+            switches('-i')
+            useWrapper()
+        }
+    }
+
+    triggers {
+        scm('* * * * *') {
+            ignorePostCommitHooks()
+        }
+    }
+
+
+    wrappers {
+        colorizeOutput()
+    }
+}
+
+job('Recruit-Service-Job') {
+    scm {
+        git {
+            remote {
+                url 'https://github.com/ositechportal/recruit-service.git'
+            }
+            extensions {
+                wipeOutWorkspace()
+            }
+            branch '*/master'
+        }
+    }
+
+    steps {
+        gradle {
+            tasks('clean')
+            tasks('test')
+            switches('-i')
+            useWrapper()
+        }
+    }
+
+    triggers {
+        scm('* * * * *') {
+            ignorePostCommitHooks()
+        }
+    }
+
+
+    wrappers {
+        colorizeOutput()
+    }
+}
+
+listView('Branch Jobs') {
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+    jobs {
+        name('Employee-Service-Job')
+        name('Recruit-Service-Job')
+    }
+}
---
+++ src/main/java/com/ositech/job_radiator.groovy
@@ -0,0 +1,7 @@
+buildMonitorView('Radiator') {
+    description('Radiator recruit service')
+    jobs {
+         name('Employee-Service-Job')
+         name('Recruit-Service-Job')
+    }
+}
